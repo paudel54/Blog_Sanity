d5e0e7ed26f3d2bd48fe9b4ab6664e7d6fe1ca39
paudel54 <2sansrit@gmail.com>
2023-05-25 18:40:29 +0545

    added google Signin SignOut Features

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import logoDark from "../public/images/logoDark.png";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Header = () => {
+  // session created under pages _app.tsx
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="w-full h-20 border-b-[1px] border-b-black font-titleFont sticky top-0 bg-white z-50 px-4">
       <div className="max-w-7xl h-full mx-auto flex justify-between items-center">
@@ -24,15 +28,34 @@ const Header = () => {
           <div className="flex items-center gap-1">
             <img
               className="w-8 h-8 rounded-full"
-              src="https://www.noormohammad.live/static/media/roundedProfile.477a194221d255c8ce26.png"
               alt="logo"
+              src={
+                session
+                  ? session?.user!.image!
+                  : // : "https://cdn-icons-png.flaticon.com/512/234/234635.png"
+                    "https://scontent.fktm10-1.fna.fbcdn.net/v/t1.6435-9/120236694_4333148533422753_9036450851618163013_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=UPVeHy3NAy4AX_TMzHJ&_nc_ht=scontent.fktm10-1.fna&oh=00_AfD7oZx-y46sBzBS7XjV_NtYo0Eiephsi4e4Bg17wXcTLA&oe=6496DDD6"
+              }
             />
-            <p className="text-sm font-medium">Hello Stranger!</p>
+            <p className="text-sm font-medium">
+              {session ? session?.user!.name : "Hello Everyone!"}
+            </p>
           </div>
 
-          <button className="uppercase text-sm border-[1px] border-primaryColor hover:border-secondaryColor px-4 py-1 font-semibold hover:text-white rounded-md hover:bg-secondaryColor transition-all duration-300 active:bg-yellow-600">
-            Sign In
-          </button>
+          {session ? (
+            <button
+              onClick={() => signOut()}
+              className="uppercase text-sm border-[1px] border-primaryColor hover:border-secondaryColor px-4 py-1 font-semibold hover:text-white rounded-md hover:bg-secondaryColor transition-all duration-300 active:bg-yellow-600"
+            >
+              Sign Out
+            </button>
+          ) : (
+            <button
+              onClick={() => signIn()}
+              className="uppercase text-sm border-[1px] border-primaryColor hover:border-secondaryColor px-4 py-1 font-semibold hover:text-white rounded-md hover:bg-secondaryColor transition-all duration-300 active:bg-yellow-600"
+            >
+              Sign In
+            </button>
+          )}
         </div>
       </div>
     </div>
